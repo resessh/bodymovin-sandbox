1180b9c19a91a230d0cd401752ba94d390bd1c99
resessh <xuesu-li@retty.me>
2017-08-18 11:10:10 +0900

    :tada: onclickでbodymovinの再生を実装

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,16 @@
-require('../css/index.css');
+import '../css/index.css';
+import '../motion/good.json';
 
-console.info('hello world');
+import * as bodymovin from 'bodymovin';
+
+const container = document.getElementById('container');
+
+const anim = bodymovin.loadAnimation({
+    container,
+    renderer: 'svg',
+    autoplay: false,
+    loop: false,
+    path: 'motion/good.json'
+});
+
+container && container.addEventListener('click', _ => anim.goToAndPlay(0, true), false);
